e7255c83b871981f37667092d3a39a50524783a7
Hampus Bosson <hampus.bosson@outlook.com>
2024-07-26 11:19:56 +0200

    Style startpage and append it to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+import "./App.css"
+import StartPage from "./components/StartPage/StartPage";
 
+function App() {
+  return (
+    < StartPage />
+  )
+}
+
+export default App;
--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,5 +1,17 @@
 import "./StartPage.css";
+import image from '../../assets/images/football-logo.png'
 
 function StartPage() {
-    return 
+    return (
+        <div className="startpage-container">
+            <img className="start-logo" src={image} alt="game-logo" />
+            <div className="difficulty-buttons">
+                <button>EASY</button>
+                <button>MEDIUM</button>
+                <button>HARD</button>
+            </div>
+        </div>
+    )
 }
+
+export default StartPage;
