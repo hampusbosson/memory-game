4213446637a25fa03a5b97d5ee30d3c1f8d9c122
Hampus Bosson <hampus.bosson@outlook.com>
2024-07-26 14:05:33 +0200

    Build basic layout for gamepage

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -1,8 +1,26 @@
 import './GamePage.css';
+import image from '../../assets/images/football-logo.png'
+import messi from '../../assets/images/player-cards/Messi.png'
 
-function GamePage() {
+function GamePage({difficulty, setDifficulty}) {
+    const toggleHomeButton = () => {
+        setDifficulty(null);
+    }
     return (
-        <div>Test</div>
+        <section className='gamepage'>
+            <div className='top-container'>
+                <img className='game-logo' src={image} alt="game-logo" onClick={toggleHomeButton}/>
+                <div className='score-box'>
+                    <p>Score: 0</p>
+                    <p>Best Score: 0</p>
+                </div>
+            </div>
+            <div className='mid-container'>
+                <div className='card-content'>
+                    <img className='card' src={messi} alt="messi-card" />
+                </div>
+            </div>
+        </section>
     )
 }
 
