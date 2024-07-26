ac7b90684ae32b87af647a5730af822e1759cfd7
Hampus Bosson <hampus.bosson@outlook.com>
2024-07-26 11:48:31 +0200

    Add logic for difficulty buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css"
 import StartPage from "./components/StartPage/StartPage";
+import { useState } from "react";
 
 function App() {
+  const [difficulty, setDifficulty] = useState(null);
+
   return (
-    < StartPage />
+    <StartPage 
+    setDifficulty={setDifficulty}
+    />
   )
 }
 
--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,14 +1,18 @@
 import "./StartPage.css";
 import image from '../../assets/images/football-logo.png'
 
-function StartPage() {
+function StartPage({ setDifficulty }) {
+    const toggleDifficulty = (difficulty) => {
+        setDifficulty(difficulty);
+    }
+
     return (
         <div className="startpage-container">
             <img className="start-logo" src={image} alt="game-logo" />
             <div className="difficulty-buttons">
-                <button>EASY</button>
-                <button>MEDIUM</button>
-                <button>HARD</button>
+                <button onClick={() => toggleDifficulty('easy')}>EASY</button>
+                <button onClick={() => toggleDifficulty('medium')}>MEDIUM</button>
+                <button onClick={() => toggleDifficulty('hard')}>HARD</button>
             </div>
         </div>
     )
