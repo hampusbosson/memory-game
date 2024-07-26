a99c6cb65984bd6f0889ccca2f60ebe7d84e39cc
Hampus Bosson <hampus.bosson@outlook.com>
2024-07-26 14:45:02 +0200

    Create PlayerCards jsx file and store all the player cards in an array to be used to render the images later

---
+++ src/assets/images/player-cards/PlayerCards.jsx
@@ -0,0 +1,35 @@
+import Bellingham from './Bellingham.png';
+import Benzema from './Benzema.png';
+import Courtois from './Courtois.png';
+import CristianoRonaldo from './Cristiano Ronaldo.png';
+import DeBruyne from './De Bruyne.png';
+import Griezmann from './Griezmann.png';
+import Haaland from './Haaland.png';
+import Ibrahimovic from './Ibra.png';
+import Kane from './Kane.png';
+import Lewandowski from './Lewandowski.png';
+import Mbappe from './Mbappe.png';
+import Messi from './Messi.png';
+import Modric from './Modric.png';
+import Neuer from './Neuer.png';
+import NeymarJr from './Neymar Jr.png';
+import Salah from './Salah.png';
+import VanDijk from './van Dijk.png';
+import ViniJr from './Vini Jr..png';
+
+const playerCards = [
+  { name: 'Bellingham', image: <img className='card' src={Bellingham} alt="Bellingham-card" /> },
+  { name: 'Cristiano Ronaldo', image: <img className='card' src={CristianoRonaldo} alt="Cristiano Ronaldo-card" /> },
+  { name: 'De Bruyne', image: <img className='card' src={DeBruyne} alt="De Bruyne-card" /> },
+  { name: 'Griezmann', image: <img className='card' src={Griezmann} alt="Griezmann-card" /> },
+  { name: 'Haaland', image: <img className='card' src={Haaland} alt="Haaland-card" /> },
+  { name: 'Ibrahimovic', image: <img className='card' src={Ibrahimovic} alt="Ibrahimovic-card" /> },
+  { name: 'Kane', image: <img className='card' src={Kane} alt="Kane-card" /> },
+  { name: 'Mbappe', image: <img className='card' src={Mbappe} alt="Mbappe-card" /> },
+  { name: 'Messi', image: <img className='card' src={Messi} alt="Messi-card" /> },
+  { name: 'Neuer', image: <img className='card' src={Neuer} alt="Neuer-card" /> },
+  { name: 'Neymar Jr', image: <img className='card' src={NeymarJr} alt="Neymar Jr-card" /> },
+  { name: 'Vini Jr', image: <img className='card' src={ViniJr} alt="Vini Jr-card" /> },
+];
+
+export default playerCards;
